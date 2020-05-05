6a6811c46b8c0e31c4735fa5f0eb6ca2d03dfe93
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2020-05-05 15:26:44 +0300

    Users routes added into index.js

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,64 @@ router.get('/',(req,res,next)=>{
     res.render('index',{title:'Express'})
 })
 
+
+/*Get /register */
+router.get('/register',(req,res,next)=>{
+    res.send('Get /register')
+})
+
+
+/*post /register */
+router.post('/register',(req,res,next)=>{
+    res.send('Post /register')
+})
+
+
+/*Get /login */
+router.get('/login',(req,res,next)=>{
+    res.send('get /login')
+})
+
+
+/*post /login */
+router.post('/login',(req,res,next)=>{
+    res.send('post /login')
+})
+
+/*get /profile */
+router.get('/profile',(req,res,next)=>{
+    res.send('get /profile')
+})
+
+/*put-update /profile */
+router.put('/profile/:user_id',(req,res,next)=>{
+    res.send('put /profile/:user_id')
+})
+
+
+/*Get /forgot-pw */
+router.get('/forgot-pw',(req,res,next)=>{
+    res.send('get /forgot-pw')
+})
+
+
+/*put /forgot-pw */
+router.put('/forgot-pw',(req,res,next)=>{
+    res.send('put /forgot-pw')
+})
+
+
+/*Get /reset-pw */
+router.get('/reset-pw/:token',(req,res,next)=>{
+    res.send('get /reset-pw/:token')
+})
+
+
+/*Get /reset-pw */
+router.put('/reset-pw/:token',(req,res,next)=>{
+    res.send('put /reset-pw/:token')
+})
+
+
+
 module.exports=router;
